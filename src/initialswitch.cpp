a80a39d8e323f83b909338f97f105690719c1dd2
andixlm <andixev@gmail.com>
2017-06-21 12:31:12 +0300

    initialSwitch: Implement ui

--- src/initialswitch.cpp
+++ src/initialswitch.cpp
@@ -1,12 +1,44 @@
+#include <QVBoxLayout>
+
 #include "initialswitch.hpp"
 
 InitialSwitch::InitialSwitch(QWidget* parent)
-    : QWidget(parent)
+    : QWidget(parent),
+      switchLayout(this)
 {
+    for (int cnt = 1; cnt <= individualHomeworkCount; ++cnt)
+    {
+        QRadioButton* rb = new QRadioButton("Individual Homework " +
+                                            QString::number(cnt));
 
+        mRadioButtons.append(rb);
+        mRadioBoxLayout.addWidget(rb);
+    }
+
+    mRadioBox.setLayout(&mRadioBoxLayout);
+
+    mStartButton.setText("Start");
+    connect(&mStartButton, &QPushButton::clicked,
+            this, &InitialSwitch::startButtonPressed);
+
+    switchLayout.addWidget(&mRadioBox);
+    switchLayout.addWidget(&mStartButton);
+
+    switchLayout.setAlignment(&mRadioBox, Qt::AlignCenter);
+    switchLayout.setAlignment(&mStartButton, Qt::AlignCenter);
 }
 
 InitialSwitch::~InitialSwitch()
+{
+    for (QVector<QRadioButton*>::const_iterator
+            iRadioButton = mRadioButtons.cbegin(), end = mRadioButtons.cend();
+         iRadioButton != end; ++iRadioButton)
+    {
+        delete *iRadioButton;
+    }
+}
+
+void InitialSwitch::startButtonPressed()
 {
 
 }
--- src/initialswitch.hpp
+++ src/initialswitch.hpp
@@ -1,6 +1,11 @@
 #ifndef INITIALSWITCH_H
 #define INITIALSWITCH_H
 
+#include <QGroupBox>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QVBoxLayout>
+#include <QVector>
 #include <QWidget>
 
 class InitialSwitch : public QWidget
@@ -10,6 +15,20 @@ class InitialSwitch : public QWidget
 public:
     InitialSwitch(QWidget* parent = Q_NULLPTR);
     ~InitialSwitch();
+
+private:
+    int individualHomeworkCount = 1;
+
+    QVBoxLayout switchLayout;
+
+    QGroupBox mRadioBox;
+    QVBoxLayout mRadioBoxLayout;
+    QVector<QRadioButton*> mRadioButtons;
+
+    QPushButton mStartButton;
+
+private slots:
+    void startButtonPressed();
 };
 
 #endif // INITIALSWITCH_HPP
