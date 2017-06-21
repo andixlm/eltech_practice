b9270bdfbfe93d2ae7cfa1e2e0d87e344bd25b30
andixlm <andixev@gmail.com>
2017-06-21 22:39:49 +0300

    ih1widget: Update task text

--- src/ih1widget.cpp
+++ src/ih1widget.cpp
@@ -16,10 +16,10 @@ IH1Widget::IH1Widget(QWidget* parent)
       mInfoLayout(&mControlWidget)
 {
     mTaskDescription.setText("Task:\n"
-                             "  Shape:\thypocycloid;\n"
-                             "  Object:\tcircle;\n"
-                             "  Fill:\t~50%;\n"
-                             "  Outine:\tset color;");
+                             "\tShape:\thypocycloid;\n"
+                             "\tObject:\tcircle;\n"
+                             "\tFill:\t~50%;\n"
+                             "\tOutine:\tset color;");
     mInfoLayout.addWidget(&mTaskDescription);
 
     mTaskImage.setPixmap(QPixmap::fromImage(QImage("res/ih1.jpg")));
