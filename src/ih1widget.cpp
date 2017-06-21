01987cb640a556f88d88c98a09f427976dcef82c
andixlm <andixev@gmail.com>
2017-06-21 17:13:50 +0300

    ih1widget: Implement ui

--- src/ih1widget.cpp
+++ src/ih1widget.cpp
@@ -1,9 +1,40 @@
+#include <QPainter>
+
 #include "ih1widget.hpp"
 
 IH1Widget::IH1Widget(QWidget* parent)
-    : QWidget(parent)
+    : QWidget(parent),
+      mMainLayout(this),
+      mParametersWidget(this),
+      mParametersLayout(&mParametersWidget)
 {
+    mTaskDescription.setText("Task:\n"
+                             "  Shape:\thypocycloid;\n"
+                             "  Object:\tcircle;\n"
+                             "  Fill:\t~50%;\n"
+                             "  Outine:\tset color;");
+    mParametersLayout.addWidget(&mTaskDescription, 0, 0);
 
+    mOuterCircleRadiusLabel.setText("Outer cicrle radius: ");
+    mOuterCircleRadius.setMinimum(MINIMUM_RADIUS);
+    mOuterCircleRadius.setMaximum(MAXIMUM_RADIUS);
+    mOuterCircleRadius.setValue(DEFAULT_RADIUS);
+    mParametersLayout.addWidget(&mOuterCircleRadiusLabel, 1, 0);
+    mParametersLayout.addWidget(&mOuterCircleRadius, 1, 1);
+
+    mInnerCircleRadiusLabel.setText("Inner circle radius: ");
+    mInnerCircleRadius.setMinimum(MINIMUM_RADIUS);
+    mInnerCircleRadius.setMaximum(MAXIMUM_RADIUS);
+    mInnerCircleRadius.setValue(DEFAULT_RADIUS / 4);
+    mParametersLayout.addWidget(&mInnerCircleRadiusLabel, 2, 0);
+    mParametersLayout.addWidget(&mInnerCircleRadius, 2, 1);
+
+    mParametersLayout.setAlignment(Qt::AlignTop | Qt::AlignHCenter);
+    mMainLayout.addWidget(&mParametersWidget);
+
+    mImageFrame.setFixedSize(IMAGE_SIZE);
+    mImageFrame.setFrameStyle(QFrame::Box);
+    mMainLayout.addWidget(&mImageFrame);
 }
 
 IH1Widget::~IH1Widget()
--- src/ih1widget.hpp
+++ src/ih1widget.hpp
@@ -1,6 +1,14 @@
 #ifndef IH1WIDGET_HPP
 #define IH1WIDGET_HPP
 
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QImage>
+#include <QLabel>
+#include <QPixmap>
+#include <QSize>
+#include <QSpinBox>
+#include <QVBoxLayout>
 #include <QWidget>
 
 class IH1Widget : public QWidget
@@ -10,6 +18,25 @@ class IH1Widget : public QWidget
 public:
     IH1Widget(QWidget* parent = Q_NULLPTR);
     ~IH1Widget();
+
+private:
+    const int DEFAULT_RADIUS = 40;
+    const int MINIMUM_RADIUS = 1;
+    const int MAXIMUM_RADIUS = 200;
+
+    const QSize IMAGE_SIZE = QSize(512, 512);
+
+    QHBoxLayout mMainLayout;
+
+    QWidget mParametersWidget;
+    QGridLayout mParametersLayout;
+    QLabel mTaskDescription;
+    QLabel mOuterCircleRadiusLabel;
+    QSpinBox mOuterCircleRadius;
+    QLabel mInnerCircleRadiusLabel;
+    QSpinBox mInnerCircleRadius;
+
+    QLabel mImageFrame;
 };
 
 #endif // IH1WIDGET_HPP
