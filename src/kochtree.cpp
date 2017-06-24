035c8a1e04be976bfdd3b7d85640e843f31101b2
andixlm <andixev@gmail.com>
2017-06-24 21:08:56 +0300

    kochNode: Implement class

--- src/kochtree.cpp
+++ src/kochtree.cpp
@@ -1,4 +1,36 @@
+#include <QLineF>
+
 #include "kochtree.hpp"
+
+KochNode::KochNode(QLineF line)
+    : mLine(line)
+{
+
+}
+
+bool KochNode::setLine(QLineF line)
+{
+    mLine = line;
+}
+
+QLineF KochNode::getLine()
+{
+    return mLine;
+}
+
+bool KochNode::addChild(KochNode* node)
+{
+    if (mChilder.count() > 3 || node == Q_NULLPTR)
+    {
+        return false;
+    }
+
+    mChilder.append(node);
+
+    return true;
+}
+
+
 
 KochTree::KochTree()
 {
--- src/kochtree.hpp
+++ src/kochtree.hpp
@@ -1,6 +1,25 @@
 #ifndef KOCHTREE_HPP
 #define KOCHTREE_HPP
 
+#include <QLineF>
+#include <QList>
+
+class KochNode
+{
+public:
+    explicit KochNode(QLineF line);
+
+    bool setLine(QLineF line);
+    QLineF getLine();
+
+    bool addChild(KochNode* node);
+    QList<KochNode*> getChildren();
+
+private:
+    QLineF mLine;
+
+    QList<KochNode*> mChilder;
+};
 
 class KochTree
 {
