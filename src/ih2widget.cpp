580c833313c937db8f1d94a48203d5624e8cda17
andixlm <andixev@gmail.com>
2017-06-24 20:50:04 +0300

    ih2widget: Implement initial ui

--- src/ih2widget.cpp
+++ src/ih2widget.cpp
@@ -1,10 +1,65 @@
+#include <QImage>
+#include <QPixmap>
+#include <QSpinBox>
 #include <QWidget>
 
 #include "ihwidget.hpp"
 #include "ih2widget.hpp"
 
 IH2Widget::IH2Widget(QWidget* parent)
-    : IHWidget(parent)
+    : IHWidget(parent),
+      mIterations(DEFAULT_ITERATIONS),
+      mFractals(DEFAULT_FRACTALS),
+      mTreeNode(DEFAULT_TREE_NODE)
 {
+    mTaskDescription.setText("Variant:\t4-4\n"
+                             "Fractal:\tKoch snowflake;\n");
+    mInfoLayout.addWidget(&mTaskDescription);
+
+    mTaskImage.setPixmap(QPixmap::fromImage(QImage("res/ih2.jpg")));
+    mInfoLayout.addWidget(&mTaskImage);
+
+    mIterationsLabel.setText("Iterations:");
+    mIterationsSpinBox.setMinimum(MINIMUM_ITERATIONS);
+    mIterationsSpinBox.setValue(DEFAULT_ITERATIONS);
+    mIterationsSpinBox.setMaximum(MAXIMUM_ITERATIONS);
+    connect(&mIterationsSpinBox,
+            static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged),
+            this, [this](int value) {
+        mIterations = value;
+    });
+    mParametersLayout.addWidget(&mIterationsLabel, 0, 0);
+    mParametersLayout.addWidget(&mIterationsSpinBox, 0, 1);
+
+    mFractalsLabel.setText("Fractals:");
+    mFractalsSpinBox.setMinimum(MINIMUM_FRACTALS);
+    mFractalsSpinBox.setValue(DEFAULT_FRACTALS);
+    mFractalsSpinBox.setMaximum(MAXIMUM_FRACTALS);
+    connect(&mFractalsSpinBox,
+            static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged),
+            this, [this](int value) {
+        mFractals = value;
+    });
+    mParametersLayout.addWidget(&mFractalsLabel, 1, 0);
+    mParametersLayout.addWidget(&mFractalsSpinBox, 1, 1);
+
+    mTreeNodeLabel.setText("Tree node:");
+    mTreeNodeSpinBox.setMinimum(MINIMUM_TREE_NODE);
+    mTreeNodeSpinBox.setValue(DEFAULT_TREE_NODE);
+    mTreeNodeSpinBox.setMaximum(MAXIMUM_TREE_NODE);
+    connect(&mTreeNodeSpinBox,
+            static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged),
+            this, [this](int value) {
+        mTreeNode = value;
+    });
+    mParametersLayout.addWidget(&mTreeNodeLabel, 2, 0);
+    mParametersLayout.addWidget(&mTreeNodeSpinBox, 2, 1);
+
+    mBuildButtonLabel.setText("Click to build:");
+    mBuildButton.setText("Build");
+    mParametersLayout.addWidget(&mBuildButtonLabel, 3, 0);
+    mParametersLayout.addWidget(&mBuildButton, 3, 1);
+
+    mInfoLayout.addLayout(&mParametersLayout);
 
 }
--- src/ih2widget.hpp
+++ src/ih2widget.hpp
@@ -1,6 +1,10 @@
 #ifndef IH2WIDGET_HPP
 #define IH2WIDGET_HPP
 
+#include <QGridLayout>
+#include <QLabel>
+#include <QPushButton>
+#include <QSpinBox>
 #include <QWidget>
 
 #include "ihwidget.hpp"
@@ -9,6 +13,39 @@ class IH2Widget : public IHWidget
 {
 public:
     explicit IH2Widget(QWidget* parent = Q_NULLPTR);
+
+private:
+    const int MINIMUM_ITERATIONS = 1;
+    const int DEFAULT_ITERATIONS = 3;
+    const int MAXIMUM_ITERATIONS = 9;
+
+    const int MINIMUM_FRACTALS = 1;
+    const int DEFAULT_FRACTALS = 2;
+    const int MAXIMUM_FRACTALS = 8;
+
+    const int MINIMUM_TREE_NODE = 0;
+    const int DEFAULT_TREE_NODE = MINIMUM_TREE_NODE;
+    const int MAXIMUM_TREE_NODE = MINIMUM_TREE_NODE;
+
+    QGridLayout mParametersLayout;
+
+    QLabel mTaskDescription;
+    QLabel mTaskImage;
+
+    int mIterations;
+    QLabel mIterationsLabel;
+    QSpinBox mIterationsSpinBox;
+
+    int mFractals;
+    QLabel mFractalsLabel;
+    QSpinBox mFractalsSpinBox;
+
+    int mTreeNode;
+    QLabel mTreeNodeLabel;
+    QSpinBox mTreeNodeSpinBox;
+
+    QLabel mBuildButtonLabel;
+    QPushButton mBuildButton;
 };
 
 #endif // IH2WIDGET_HPP
