ae35930af53f459fbd2d25d82dffd40d22bdbdf6
andixlm <andixev@gmail.com>
2017-06-25 13:48:22 +0300

    tools: Implement methods to get equilateral triangle

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,5 +1,8 @@
 #include <QImage>
+#include <QList>
 #include <QPainter>
+#include <QPointF>
+#include <QtMath>
 
 #include "tools.hpp"
 
@@ -21,6 +24,67 @@ QImage Tools::getPlaneImage(int width, int height)
 QImage Tools::getPlaneImage(QSize size)
 {
     return getPlaneImage(size.width(), size.height());
+}
+
+QImage Tools::getEquilateralTriangle(int width, int height)
+{
+    QImage image = QImage(width, height, QImage::Format_RGB32);
+    image.fill(Qt::white);
+
+    QList<QLineF> lines = getEquilateralTriangleLines(width, height);
+
+    QPainter painter;
+    painter.begin(&image);
+    for (auto line = lines.cbegin(), listEnd = lines.cend();
+         line != listEnd; ++line)
+    {
+        painter.drawLine(*line);
+    }
+    painter.end();
+
+    return image;
+}
+
+QImage Tools::getEquilateralTriangle(QSize size)
+{
+    return getEquilateralTriangle(size.width(), size.height());
+}
+
+QList<QLineF> Tools::getEquilateralTriangleLines(int width, int height)
+{
+    QList<QLineF> lines;
+    QLineF lineOne, lineTwo, lineThree;
+    double lineLength = static_cast<double>(width) - 200.0;
+    double triangleHeight = lineLength * qCos(qDegreesToRadians(30.0));
+
+    lineOne.setP1(QPointF(static_cast<double>(width) - 100.0,
+                          static_cast<double>(height) / 2.0 +
+                          triangleHeight / 2.0));
+    lineOne.setLength(lineLength);
+    lineOne.setAngle(180.0);
+
+    lineTwo.setP1(QPointF(100.0,
+                          static_cast<double>(height) / 2.0 +
+                          triangleHeight / 2.0));
+    lineTwo.setLength(lineLength);
+    lineTwo.setAngle(60.0);
+
+    lineThree.setP1(QPointF(static_cast<double>(width) - 100.0,
+                            static_cast<double>(height) / 2.0 +
+                            triangleHeight / 2.0));
+    lineThree.setLength(lineLength);
+    lineThree.setAngle(120.0);
+
+    lines.append(lineOne);
+    lines.append(lineTwo);
+    lines.append(lineThree);
+
+    return lines;
+}
+
+QList<QLineF> Tools::getEquilateralTriangleLines(QSize size)
+{
+    return getEquilateralTriangleLines(size.width(), size.height());
 }
 
 int Tools::gcd(int alpha, int beta)
--- src/tools.hpp
+++ src/tools.hpp
@@ -2,6 +2,8 @@
 #define TOOLS_HPP
 
 #include <QImage>
+#include <QLineF>
+#include <QList>
 #include <QSize>
 
 class Tools
@@ -9,6 +11,11 @@ class Tools
 public:
     static QImage getPlaneImage(int width, int height);
     static QImage getPlaneImage(QSize size);
+
+    static QImage getEquilateralTriangle(int width, int height);
+    static QImage getEquilateralTriangle(QSize size);
+    static QList<QLineF> getEquilateralTriangleLines(int width, int height);
+    static QList<QLineF> getEquilateralTriangleLines(QSize size);
 
     static int gcd(int alpha, int beta);
 };
