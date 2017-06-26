acec7a11fbe767f6beb613def5183055ea23c0e3
andixlm <andixev@gmail.com>
2017-06-26 13:12:38 +0300

    kochLine: Remove methods inline modifier

--- src/kochline.hpp
+++ src/kochline.hpp
@@ -10,26 +10,26 @@ public:
     KochLine(double x = 0.0, double y = 0.0,
              double length = 0.0, double angle = 0.0);
 
-    inline QPointF getP1();
-    inline void setP1(QPointF point);
+    QPointF getP1();
+    void setP1(QPointF point);
 
-    inline double getX1();
-    inline void setX1(double x);
-    inline double getY1();
-    inline void setY1(double y);
+    double getX1();
+    void setX1(double x);
+    double getY1();
+    void setY1(double y);
 
-    inline QPointF getP2();
+    QPointF getP2();
 
-    inline double getX2();
-    inline double getY2();
+    double getX2();
+    double getY2();
 
-    inline double getLength();
-    inline void setLength(double length);
+    double getLength();
+    void setLength(double length);
 
-    inline double getAngle();
-    inline void setAngle(double angle);
+    double getAngle();
+    void setAngle(double angle);
 
-    inline QLineF getLine();
+    QLineF getLine();
 
 private:
     QPointF mP1;
